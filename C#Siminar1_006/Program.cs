68bd3d15a53eb5d42adf0ac42ac2859be53c7d6a
Maksim <dumkin_m@rambler.ru>
2022-06-18 11:30:35 +0300

    C# workshop 1 task 2

---
+++ C#Siminar1_006/Program.cs
@@ -0,0 +1,13 @@
+﻿//the program for finding an even number
+int numbers, krats;
+krats=2;
+Console.Write("Enter number ");
+numbers = Convert.ToInt32(Console.ReadLine()) ;
+if (numbers % krats==0 ) //parity check
+    {
+        Console.WriteLine(numbers+"-> Yes");
+    }   
+    else
+        {
+             Console.WriteLine(numbers+"-> No"); 
+        }
