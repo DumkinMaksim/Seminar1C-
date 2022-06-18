bca8796607a5ce01bd0f2d76ba0d1a2b4dec7009
Maksim <dumkin_m@rambler.ru>
2022-06-18 11:35:46 +0300

    C# Workshop 1 Task 2

---
+++ C#Siminar1_002/Program.cs
@@ -0,0 +1,24 @@
+﻿// min and max search program from two numbers
+int  a, b;
+Console.Write("Input a= ") ;
+a = Convert.ToInt32(Console.ReadLine()) ;
+Console.Write("Input b= ") ;
+b = Convert.ToInt32(Console.ReadLine()) ;
+if (a>b)
+ {
+    Console.WriteLine("Max= "+a) ;
+    Console.WriteLine("Min= "+b) ;
+}
+else 
+{
+    if (a==b) 
+    {
+    Console.WriteLine("no min or max, the numbers are equal");
+    Console.WriteLine(a+"="+b);
+  }
+  else
+    {
+    Console.WriteLine("Max= "+b) ;
+    Console.WriteLine("Min= "+a) ;
+    }
+}
