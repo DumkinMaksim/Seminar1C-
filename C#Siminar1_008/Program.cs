491a852d4cd56e4b79d58db29a96a744b5a484da
Maksim <dumkin_m@rambler.ru>
2022-06-18 11:25:24 +0300

    c# workshop 1 task 8

---
+++ C#Siminar1_008/Program.cs
@@ -0,0 +1,14 @@
+﻿int  a, index;
+Console.Write("Input N= ") ;
+a = Convert.ToInt32(Console.ReadLine()) ;
+ for (index = 1; index<a+1; index++)
+    {
+        if (index==1)
+        {
+            Console.WriteLine("even numbers not found "); 
+        }
+        if (index % 2==0)
+        {
+             Console.Write(index+"  "); 
+        }
+    }
