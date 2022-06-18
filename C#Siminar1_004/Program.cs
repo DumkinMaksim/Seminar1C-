e9f770c3a5320ed4bc994cc940a3482f736f924d
Maksim <dumkin_m@rambler.ru>
2022-06-18 11:32:06 +0300

    C# Workshop 1 task 4

---
+++ C#Siminar1_004/Program.cs
@@ -0,0 +1,26 @@
+﻿
+        // search program max numbers out of three
+       int [] a= new int [3];
+        int i ;
+        int max = a[0];
+        Console.Write("Enter first number ");
+        a[0] = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter second numbers ");
+        a[1] = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the third number ");
+        a[2] = Convert.ToInt32(Console.ReadLine());
+         for (i = 0; i<3; i++)
+          {
+         {
+            if (a[i] > a[i++])
+            {
+              max=a[i];  
+            } 
+               else 
+               { 
+                max = a[i++];    
+        }
+        Console.WriteLine("Max="+max);
+         }
+          }
+          
